6cac6b43852ca07ff0b42f895b3d7e02f6db3177
simon Garrelou <1513048@rgu.ac.uk>
2015-11-23 13:51:09 +0000

    retrieve

--- retrieve.js
+++ retrieve.js
@@ -16,25 +16,23 @@ http.createServer(function(request, response) {
             db.close();
         } else {
             //HURRAY!! We are connected. :)
-            response.write('Connection established to ' + url + "\n");
+            response.write('Connection established to' + url + "\n");
 
             // do some work here with the database.
             var collection = db.collection("users");
-            var user1 = {name: "modulus admin", age: 42, roles: ["admin", "moderator", "user"]};
-            var user2 = {name: 'modulus user', age: 22, roles: ['user']};
-            var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
+            var result = collection.find({name: "modulus user"});
 
-            collection.insert([user1, user2, user3], function (err, result) {
+            results.each(function (err, result) {
                 if (err) {
-                    response.write('Insert failed: ' + err + "\n");
+                    response.write("Error retrieving: " + err + "\n");
                 } else {
-                    console.log(result);
-                    response.write('Inserted ' + result.insertedCount + ' documents ok. \n');
+                    response.write("Fetched " + result.name + " : " + result.age + " y/o, roles " + result.roles.toString() + "\n");
                 }
-                //Close connection
-                db.close();
-                response.end('Finished, Connection closed \n');
-                //remove any other db.close or response.end statement below this line
+
+                if (result == null) {
+                    response.end("End retrieve\n");
+                    db.close();
+                }
             });
         }
     });
