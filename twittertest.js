ae6fdfc86ff4dd1a239790d8c213f36b21529577
simon Garrelou <1513048@rgu.ac.uk>
2015-11-30 13:13:54 +0000

    add a request

--- twittertest.js
+++ twittertest.js
@@ -12,7 +12,7 @@ var client = new Twitter({
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
-    client.get("search/tweets", {q: ""}, function (error, tweets) {
+    client.get("search/tweets", {q: "firefox"}, function (error, tweets) {
         if (error) {
             throw error;
         } else {
