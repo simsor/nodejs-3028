47642d5d39e8e27f9d521163469261491f7cb1be
simon Garrelou <1513048@rgu.ac.uk>
2015-11-30 13:35:47 +0000

    add twitter handle

--- twittertest.js
+++ twittertest.js
@@ -19,7 +19,7 @@ http.createServer(function(request, response) {
         } else {
             var json = [];
             for(var i=0; i < tweets.statuses.length; i++) {
-                json.push({name: tweets.statuses[i].user.name, text: tweets.statuses[i].text});
+                json.push({name: tweets.statuses[i].user.name, text: tweets.statuses[i].text, handle: tweets.statuses[i].user.screen_name});
             }
 
             response.end(JSON.stringify(json));
